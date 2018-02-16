1a6fa9de9dc12dd4f8a50341f9443d7116f56ab9
davlis <ddewid@gmail.com>
2018-02-16 03:22:51 +0100

    Hello route to routes.

--- routes.js
+++ routes.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const statsRoutes = require('./routes/stats');
 
 router.use('/stats', statsRoutes);
+router.use('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config.js');
+const routes = require('./routes');
+
+app.use('/', routes);
 
 mongoose.connect(config.mongo.url, { autoReconnect: true });
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('App listening on port 3000!'));
