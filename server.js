c0fa63e0295cfe32821a22eabc8b81644f199d29
davlis <ddewid@gmail.com>
2018-02-16 04:01:57 +0100

    Added morgan for loggin.

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('./config.js');
 const routes = require('./routes');
 
+app.use(morgan('dev'));
 app.use(bodyparser.json({ limit: '1mb' }));
 app.use(bodyparser.urlencoded({ limit: '1mb', parameterLimit: 50000, extended: true }));
 app.use(cors());
